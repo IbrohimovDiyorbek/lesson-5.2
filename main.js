ba7503aeb340b63ea91f92e074e1344be9e25d07
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-04-17 21:40:33 +0500

    it is a homework

---
+++ main.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingizni kiriting!");
+let id = prompt("Space ID yingizni kiriting!");
+let parol = prompt("Space password ingizni kiriting!");
+
+const diyorbek = {
+    name: ism,
+    id: id,
+    password: parol,
+};
+console.log(diyorbek);
